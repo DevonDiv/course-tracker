dde5ae81992d651e496faac6b9605090c0d8ac1d
Devon Divinecz <w0723299@myscc.ca>
2021-12-22 01:51:28 -0500

    Added input value for current course for edits, tabs get loaded with ngFor

--- src/app/components/tracker-tabs/tracker-tabs.component.ts
+++ src/app/components/tracker-tabs/tracker-tabs.component.ts
@@ -1,11 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Course } from 'src/app/services/course.model';
+
 @Component({
   selector: 'app-tracker-tabs',
   templateUrl: './tracker-tabs.component.html',
   styleUrls: ['./tracker-tabs.component.css']
 })
 export class TrackerTabsComponent implements OnInit {
+
+courses: Course[] = [
+  // {
+  //   courseName: 'Angular',
+  //   profName: 'John Doe',
+  //   profEmail: 'doe@gmail.com'
+  // },
+  // {
+  //   courseName: 'Framework',
+  //   profName: 'John Doe',
+  //   profEmail: 'doe@gmail.com'
+  // },
+  // {
+  //   courseName: 'iOS',
+  //   profName: 'John Doe',
+  //   profEmail: 'doe@gmail.com'
+  // }
+]
 
   constructor() { }
 
