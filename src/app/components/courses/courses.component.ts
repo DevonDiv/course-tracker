99ab054048e4dca8544e74686b18894777596f8a
Devon Divinecz <w0723299@myscc.ca>
2021-12-21 23:31:09 -0500

    Added form submission functionality for adding courses

--- src/app/components/courses/courses.component.ts
+++ src/app/components/courses/courses.component.ts
@@ -1,17 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { Course } from '../../services/course.model';
+import { CourseService } from '../../services/course.service';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.css']
 })
-export class CoursesComponent implements OnInit {
+export class CoursesComponent implements OnInit, OnDestroy {
 
   hide: boolean = false;
+  courses: Course[] = [];
+  private courseSubscription: Subscription = new Subscription;
 
-  constructor() { }
+  constructor(public courseService: CourseService) { }
 
   ngOnInit(): void {
+    this.courses = this.courseService.getCourses();
+    this.courseSubscription = this.courseService.getCourseUpdateListener()
+    .subscribe((courses: Course[]) => { this.courses = courses; }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.courseSubscription.unsubscribe();
   }
 
   editCourse() {
